f0d4a55a0bd7bf3589037745619060c4ec2d29b5
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-08-28 19:05:30 -0700

    vue add vuetify

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
-        path: path.resolve(_direname, "dist")
+        path: path.resolve(_dirname, "dist")
     },
     modules: {
         rules: [
